f46269a06368edba3d3d9bce6ca2ab6a1d2bb2e9
mandriic <kellaxxx@gmail.com>
2024-01-31 13:30:02 +0100

    Change content

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,10 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Welcome to Streamlit Nicolas PortPage!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+Is only example, usage of Python3 with Streamlit  :heart:.
+More content soon... 
 """
 
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
